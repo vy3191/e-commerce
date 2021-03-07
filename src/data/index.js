74b70c26fc5dd483f42e871daf1f7c6529ae5ec6
vy3191 <venky-yagatilee@lambdastudents.com>
2021-03-07 14:44:37 -0500

    modified cars data

--- src/data/index.js
+++ src/data/index.js
@@ -3,48 +3,56 @@ export const cars = [
   year:2013, 
   make: 'Mazda', 
   model:'CX-5',
+  price: 26395,
   id:1
 },
 {
   year:2013, 
   make: 'Mazda', 
   model:'MAZDA6',
+  price: 28395,
   id:2
 },
 {
   year:2013, 
   make: 'Nissan',
   model: 'Altima',
+  price: 24000,
   id:3
 },
 {
   year:2013, 
   make: 'Nissan',
   model: 'GT-R',
+  price: 26000,
   id:4
 },
 {
   year:2013, 
   make: 'Porsche',
   model: '911',
+  price: 32500,
   id: 5
 },
 {
   year:2013, 
   make: 'Porsche',
   model:'Boxster',
+  price: 34000,
   id: 6
 },
 {
   year:2013, 
   make: 'Porsche',
   model:'Cayenne',
+  price: 31000,
   id: 7
 },
 {
   year:2013, 
   make: 'Porsche',
   model:'Panamera',
+  price: 29500,
   id: 8
 },
 
@@ -52,54 +60,63 @@ export const cars = [
   year:2013, 
   make: 'Scion', 
   model:'FR-S',
+  price: 278500,
   id: 10
 },
 {
   year:2013, 
   make: 'Scion', 
   model:'tC',
+  price: 283200,
   id: 11
 },
 {
   year:2013, 
   make: 'Subaru',
   model: 'BRZ',
+  price: 19500,
   id: 12
 },
 {
   year:2013, 
   make: 'Toyota',
   model: 'Land Cruiser',
+  price: 21000,
   id: 13
 },
 {
   year:2013, 
   make: 'Toyota',
   model: 'Venza',
+  price: 23400,
   id: 14
 },
 
  { year: 2013, 
   make: 'Volvo', 
   model:'C30',
+  price: 21900,
   id: 15
 },
 {
   year:2013, 
   make: 'Volvo', 
   model:'C70',
+  price: 22450,
   id: 16
 },
 {
   year:2013, 
   make: 'Volvo', 
   model:'S60',
+  price: 23890,
   id: 17
 },
 {
   year:2013, 
   make: 'Volvo', 
   model:'XC90',
+  price: 22450,
   id: 18
 }
 ]
