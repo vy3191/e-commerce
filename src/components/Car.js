cfc45ff640bb50124636d474dc5409f68f4967bc
vy3191 <venky-yagatilee@lambdastudents.com>
2021-03-07 14:37:59 -0500

    Modified Cards component styles

--- src/components/Car.js
+++ src/components/Car.js
@@ -8,10 +8,10 @@ class Car extends Component {
       <div className="card">
         {/* <img src="img_avatar.png" alt="Avatar" style="width:100%" /> */}
         <div className=" ">
-          <h1><b>{car.make}</b></h1>
-          <h2>{car.model}</h2>
-          <p>{car.year}</p>
-          <p>{car.id}</p>
+          <p>Serial Number: {car.id}</p>
+          <h3>Car Make: <b>{car.make}</b></h3>
+          <h5>Car Model: {car.model}</h5>
+          <h5>Year: {car.year}</h5>
         </div>
       </div>
     );
