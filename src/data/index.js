77404efb34168f53797d4f05fef614304abbfeeb
vy3191 <venky-yagatilee@lambdastudents.com>
2021-03-06 22:51:50 -0500

    Added cars data

---
+++ src/data/index.js
@@ -0,0 +1,109 @@
+ cars = [
+{
+  year:2013, 
+  make: 'Mazda', 
+  model:'CX-5',,
+  id:1
+},
+{
+  year:2013, 
+  make: 'Mazda', 
+  model:'MAZDA6',
+  id:2
+},
+{
+  year:2013, 
+  make: 'Nissan',
+  model: 'Altima',
+  id:3
+},
+{
+  year:2013, 
+  make: 'Nissan',
+  model: 'GT-R',
+  id:4
+},
+{
+  year:2013, 
+  make: 'Porsche',
+  model: '911',
+  id: 5
+},
+{
+  year:2013, 
+  make: 'Porsche',
+  model:'Boxster',
+  id: 6
+}
+{
+  year:2013, 
+  make: 'Porsche',
+  model:'Cayenne',
+  id: 7
+},
+{
+  year:2013, 
+  make: 'Porsche',
+  model:'Panamera',
+  id: 8
+},
+
+{
+  year:2013, 
+  make: 'Scion', 
+  model:'FR-S',
+  id: 10
+},
+{
+  year:2013, 
+  make: 'Scion', 
+  model:'tC',
+  id: 11
+},
+{
+  year:2013, 
+  make: 'Subaru',
+  model: 'BRZ',
+  id: 12
+},
+{
+  year:2013, 
+  make: 'Toyota',
+  model: 'Land Cruiser',
+  id: 13
+},
+{
+  year:2013, 
+  make: 'Toyota',
+  model: 'Venza',
+  id: 14
+},
+
+ { year: 2013, 
+  make: 'Volvo', 
+  model:'C30',
+  id: 15
+},
+{
+  year:2013, 
+  make: 'Volvo', 
+  model:'C70',
+  id: 16
+},
+{
+  year:2013, 
+  make: 'Volvo', 
+  model:'S60',
+  id: 17
+},
+{
+  year:2013, 
+  make: 'Volvo', 
+  model:'XC90',
+  id: 18
+}
+]
+
+module.export = {
+  cars
+}
