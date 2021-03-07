d683a6b29274d7d20f95fe7c95068f792d827a9f
vy3191 <venky-yagatilee@lambdastudents.com>
2021-03-06 20:39:13 -0500

    Adds Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,36 @@
 import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
 
 class Header extends Component {
   render() {
     return (
-      <div>
-        
-      </div>
+      <nav className="navbar navbar-default">
+        <div className="container-fluid">
+          <div className="navbar-header">
+            <NavLink  
+              activeClassName="selected"
+              className="navbar-brand"
+            >CAR SALES
+            </NavLink>
+          </div>
+          <ul className="nav navbar-nav">
+            <li>
+              <NavLink  
+                activeClassName="selected"
+                className="navbar-brand"
+              >About
+              </NavLink>
+            </li>
+            <li>
+              <NavLink  
+                activeClassName="selected"
+                className="navbar-brand"
+              >Contact Us
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </nav>
     );
   }
 }
