766e040a62befa7608cd048f2bde2d9ad5c356d1
vy3191 <venky-yagatilee@lambdastudents.com>
2021-03-08 14:34:23 -0500

    Added contact compoennt added contact component styles

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,7 +3,16 @@ import React, { Component } from 'react';
 class Contact extends Component {
   constructor(props) {
     super(props);
-    
+    this.state = {
+      firstName: '',
+      lastName: '',
+      email: '',
+      subject: ''
+    }
+
+    this.handleInput = this.handleInput.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+
   }
 
   handleInput(event) {
@@ -11,17 +20,39 @@ class Contact extends Component {
       [event.target.name]: event.target.value
     })
   }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    const { firstName, lastName, email, subject } = this.state;
+    const personalDetails = {
+      firstName, 
+      lastName, 
+      email, 
+      subject
+    }
+    console.log('personalObject', personalDetails);
+    this.setState({
+      firstName: '',
+      lastName: '',
+      email: '',
+      subject: ''
+    })
+  }
   render() {
+    const { firstName, lastName, email, subject } = this.state;
+    console.log('state>>', this.state)
     return (
       <div>
         <h1>Contact Details</h1>
         <div className="container">
-          <form>
+          <form onSubmit={ this.handleSubmit }>
             <label htmlFor="fname">First Name</label>
             <input 
               type="text" 
               id="fname" 
               name="firstName" 
+              value={ firstName }
+              onChange={ this.handleInput}
               placeholder="Your name.." 
             />
             <label htmlFor="lname">Last Name</label>
@@ -29,26 +60,35 @@ class Contact extends Component {
               type="text" 
               id="lname"
               name="lastName" 
+              value={ lastName }
+              onChange={ this.handleInput}
               placeholder="Your last name.." 
             />
-            <label htmlFor="country">Country</label>
+            <label htmlFor="email">Email</label>
             <input 
               type="email" 
               id="email"
               name="email" 
+              value={ email }
+              onChange={ this.handleInput}
               placeholder="Your email.." 
             />
             <label htmlFor="subject">Subject</label>
             <textarea 
               id="subject" 
               name="subject" 
+              value={ subject }
+              onChange={ this.handleInput}
               placeholder="Write something.." 
               style={{height:"200px"}} />
-
-            <input 
-              type="button" 
-              value="Submit"               
-            />
+            <div className="submit">
+               <button 
+                disabled={ !firstName || !lastName || !email || !subject }
+               className="btn btn-lg btn-success"
+              >
+                Submit
+              </button>
+            </div>            
           </form>
         </div>
       </div>
